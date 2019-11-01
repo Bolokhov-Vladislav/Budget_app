b83ef3fb6d380e76972734afbfb44f57529adb48
bolohov <bolohov.vlad.95@gmail.com>
2019-11-01 17:40:26 +0200

    initialization

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+let start = document.getElementById('start'),
+
+    budgetValue = document.getElementsByClassName('budget-value')[0],
+    levelValue = document.getElementsByClassName('level-value')[0],
+    expensesValue = document.getElementsByClassName('expenses-value')[0],
+    optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+    incomeValue = document.getElementsByClassName('income-value')[0],
+    monthsavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+    yearsavingsValue = document.getElementsByClassName('yearsavings-value'),
+
+    expensesItem = document.getElementsByClassName('expenses-item'),
+    expensesBtn = document.getElementsByTagName('button')[0],
+
+    optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    optionalexpensesBtn = document.getElementsByTagName('button')[1],
+
+    countBudgetBtn = document.getElementsByTagName('button')[2],
+
+    chooseIncome = document.querySelector('#income'),
+    savings = document.querySelector('#savings'),
+    chooseSum = document.querySelector('#sum'),
+    choosePercent = document.querySelector('#choose-percent');
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
+
